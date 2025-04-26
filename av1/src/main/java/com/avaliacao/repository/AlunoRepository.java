baf3f1569b250ceab32dc82104c384c43fd3193e
clesiosalmeida <69396527+clesiosalmeida@users.noreply.github.com>
2025-04-25 22:42:48 -0300

    Delete av1 directory

--- av1/src/main/java/com/avaliacao/repository/AlunoRepository.java
+++
@@ -1,5 +0,0 @@
-import com.avaliacao.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {}
--- av1/src/main/java/com/avaliacao/repository/CursoRepository.java
+++
@@ -1,4 +0,0 @@
-import com.avaliacao.entity.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {}
